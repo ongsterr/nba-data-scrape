385163e3eab8cdaef6acefbd265a660f19231ac4
Chris Ong <ong.chris11@gmail.com>
2019-01-05 10:10:33 +1100

    feat: add data aggregate feature

--- src/repo/playerProj.js
+++ src/repo/playerProj.js
@@ -40,10 +40,80 @@ const playerProjRepo = () => {
       },
     }).lean()
 
+  const aggregateData = async (startDate, endDate) =>
+    await PlayerProj.aggregate([
+      {
+        $lookup: {
+          from: 'playerstats',
+          let: { first_concat: '$concat' },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $and: [{ $eq: ['$concat', '$$first_concat'] }],
+                },
+              },
+            },
+            {
+              $project: {
+                _id: 0,
+                __v: 0,
+                date: 0,
+                team: 0,
+                opp: 0,
+                playerId: 0,
+                playerName: 0,
+              },
+            },
+          ],
+          as: 'actuals',
+        },
+      },
+      {
+        $lookup: {
+          from: 'playerdfsprojs',
+          let: { first_date: '$date', first_player: '$playerId' },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $and: [
+                    { $eq: ['$date', '$$first_date'] },
+                    { $eq: ['$playerId', '$$first_player'] },
+                  ],
+                },
+              },
+            },
+            {
+              $project: {
+                _id: 0,
+                __v: 0,
+                date: 0,
+                team: 0,
+                opp: 0,
+                playerId: 0,
+                playerName: 0,
+              },
+            },
+          ],
+          as: 'dfs',
+        },
+      },
+      {
+        $match: {
+          date: {
+            $gte: moment(startDate, 'DD-MM-YYYY').toDate(),
+            $lte: moment(endDate, 'DD-MM-YYYY').toDate(),
+          },
+        },
+      },
+    ])
+
   return {
     saveData,
     checkData,
     getData,
+    aggregateData,
   }
 }
 
